585ac08ef26c860745d308fbee31b5b7091d0e18
Ruben <ruben.abellan@ext.leroymerlin.es>
2019-07-29 17:49:43 +0200

    Commit inicial

---
+++ app.js
@@ -0,0 +1,29 @@
+// Ejecuctar: node app --base=3
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+console.log(argv);
+let comando = argv._[0];
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(colors.yellow(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('comando no reconocido');
+}
+
+console.log(argv);
+console.log(` base: ${argv.base} limite: ${argv.limite}`);
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+
+// console.log(parametro)
